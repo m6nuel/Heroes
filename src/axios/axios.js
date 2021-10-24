11b8f43d1fcb38d5c5196e95c15f7bfa2fd86bdb
Jesus Delgado <m6nuel@gmail.com>
2021-10-24 20:42:46 -0300

    prueba de la peticion

--- src/axios/axios.js
+++ src/axios/axios.js
@@ -1,16 +1,24 @@
 import axios from "axios";
+// import { useDispatch } from "react-redux";
+// import { login } from "../actions/auth";
 
+// export const autenticacion = async ( email, password ) => {
+//     const dispatch = useDispatch();
+//     try {
+//         const peticion = await axios.post('http://challenge-react.alkemy.org/', {'email': email,'password': password});
 
-export const autenticacion = async ( email, password ) => {
-    try {
-        const peticion = await axios.post('http://challenge-react.alkemy.org/', {'email': email,'password': password});
+//         const { token } = peticion.data;
+//         if ( token ) {
+//             localStorage.setItem( 'token', token )
+//             dispatch( login( !!token ) )
+//         }
+//     } catch (error) {
+//         console.log('Credenciales Incorrectas')
+//     }
+// }
 
-        const { token } = peticion.data;
-        if ( token ) {
-            localStorage.setItem( 'token', token )
-        }
-        diskpatch( login( !!token ) )
-    } catch (error) {
-        console.log('Credenciales Incorrectas')
-    }
+export const getHeroes = async (name) => {
+    return await axios({
+        url: `https://superheroapi.com/api.php/10227697953733690/search/${name}`
+    })
 }
--- src/components/homeScreen/HomeScreen.js
+++ src/components/homeScreen/HomeScreen.js
@@ -1,11 +1,46 @@
+
 import React from 'react';
+import { getHeroes } from '../../axios/axios';
 import { NavBar } from './NavBar';
 
 export const HomeScreen = () => {
+    // url= 'http://superheroapi.com/api.php/10227697953733690/${id}'
+
+
+
+getHeroes('po').then( heroe => console.log(heroe))
+
 
     return (
         <>
             <NavBar />
+
+            <div>
+                <div className="container">
+                    <div className="row">
+                        <div className="col">
+                            1 of 3
+                        </div>
+                        <div className="col">
+                            2 of 3
+                        </div>
+                        <div className="col">
+                            3 of 3
+                        </div>
+                    </div>
+                    <div className="row">
+                        <div className="col">
+                            1 of 3
+                        </div>
+                        <div className="col">
+                            2 of 3
+                        </div>
+                        <div className="col">
+                            3 of 3
+                        </div>
+                    </div>
+                </div>
+            </div>
         </>
     )
 }
